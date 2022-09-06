c2b05b8394a6104edce1c627b721c89eac47063b
Stella Dimopoulou <stella.dim@hotmail.com>
2022-09-06 16:00:31 +0300

    v4 of dicom structured content anonymization library.

--- src/main/java/dummy/Main.java
+++ src/main/java/dummy/Main.java
@@ -12,17 +12,6 @@ public class Main {
 
         String base64Str = "";
 
-        /*String path = "C:\\Users\\user\\Downloads\\Encoded_Base64_DICOM.txt";
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
-        String currentLine = "";
-        StringBuilder stringBuilder = new StringBuilder();
-        while((currentLine = bufferedReader.readLine()) != null) {
-            stringBuilder.append(currentLine.trim());
-        }
-        bufferedReader.close();
-        base64Str = stringBuilder.toString();
-        System.out.println("Encoded Base64 String: " + base64Str);*/
-
         DICOMAnonymization dicomAnon = new DICOMAnonymization("http://localhost:8000");
         String response = dicomAnon.request(base64Str);
         System.out.println("Response: " + response);
